f4f2bed114c983b0ec767530284ca0e6abe17761
PPHamster <shinnapatjr@gmail.com>
2022-07-07 20:31:42 +0700

    Setting default timezone Thailand

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/globals.css'
 import '../styles/normalize.css'
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+/* Config timezone to Thailand time */
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('Asia/Bangkok');
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
